9d35699f4ba6daf7ade5b7551c3576c568882b35
Meredith Heller <meredith.a.heller@gmail.com>
2016-08-15 14:48:18 -0700

    add article validations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,31 @@
 class Article < ActiveRecord::Base
+  BOOLEAN_VALUES = [true, false]
+  ARTICLE_TYPES = ["top", "bottom", "outerwear", "shoes", "dress", "misc"]
+  validates :user_id, :article_type, :title, :temp_min, :temp_max, :formality, presence: true
+  validates :rain, :clouds, :snow, :wind, inclusion: BOOLEAN_VALUES
+  validates :article_type, inclusion: ARTICLE_TYPES
+  validate :valid_temp_range
+  validate :valid_formality_range
+
+  belongs_to :user
+
+  def valid_temp_range
+    unless temp_min.between?(1,5)
+      errors[:temp_min] << "Invalid minimum temperature"
+    end
+    unless temp_max.between?(1,5)
+      errors[:temp_max] << "Invalid maximum temperature"
+    end
+    if temp_min > temp_max
+     errors[:temp_range] << "Maximum temperature cannot be lower than minimum temperature"
+    end
+  end
+
+  def valid_formality_range
+    unless formality.between?(1,10)
+      errors[:formality] << "Invalid formality input"
+    end
+  end
+
+
 end
