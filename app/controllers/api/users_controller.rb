af2779b768a6f0eb03cdb16ae30e5f9bcafc17dc
Meredith Heller <meredith.a.heller@gmail.com>
2016-08-17 14:06:14 -0700

    add session controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,7 +1,23 @@
 class Api::UsersController < ApplicationController
   def create
+    @user = User.new(user_params)
+    if @user.save
+      log_in!(@user)
+    else
+      render json: @user.errors.full_messages, status: 422
   end
 
   def update
+    user = current_user
+    if user.update!(user_params)
+      render json: "to be filled in"
+    else
+      render json: @user.errors.full_messages, status: 422
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name,:email,:password,:temp_offset,:zipcode)
   end
 end
