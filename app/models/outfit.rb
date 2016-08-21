061d13b7566077d224367afb61ceb8e72174b689
Meredith Heller <meredith.a.heller@gmail.com>
2016-08-21 12:18:56 -0700

    seed data

--- app/models/outfit.rb
+++ app/models/outfit.rb
@@ -1,10 +1,33 @@
 class Outfit < ActiveRecord::Base
   #add validations
-  validates :user_id, :temp_min, :temp_max, :rain, :clouds, :snow, :wind, :formality, :title
-  has_many :compositions
-  has_many :articles, through: :compositions
+  BOOLEAN_VALUES = [true, false]
+  validates :user_id, :temp_min, :temp_max, :rain, :title, presence: true
+  validates :rain, :clouds, :snow, :wind, inclusion: BOOLEAN_VALUES
+  validate :valid_temp_range
+  validate :valid_formality_range
+
   before_create :set_title
 
+  has_many :compositions
+  has_many :articles, through: :compositions
+
+  def valid_temp_range
+    unless temp_min.between?(1,5)
+      errors[:temp_min] << "Invalid minimum temperature"
+    end
+    unless temp_max.between?(1,5)
+      errors[:temp_max] << "Invalid maximum temperature"
+    end
+    if temp_min > temp_max
+     errors[:temp_range] << "Maximum temperature cannot be lower than minimum temperature"
+    end
+  end
+
+  def valid_formality_range
+    unless formality.between?(1,10)
+      errors[:formality] << "Invalid formality input"
+    end
+  end
   def set_title
     self.title = "#OOTD #{Date.today.to_formatted_s(:long)}"
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,13 +7,95 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 user = User.create!(password: "123123", email: "hi", name: "chris", temp_offset: 0, zipcode: "94015")
-a1 = Article.create!(user_id: user.id, article_type: "top", title: "top1", formality: 3, rain: true, clouds: true, snow: false, wind: true)
-a2 = Article.create!(user_id: user.id, article_type: "bottom", title: "bottom1", formality: 4, rain: false, clouds: true, snow: false, wind: false)
-a3 = Article.create!(user_id: user.id, article_type: "outerwear", title: "outerwear1", formality: 3, rain: true, clouds: true, snow: false, wind: true)
-a4 = Article.create!(user_id: user.id, article_type: "top", title: "top2", formality: 1, rain: true, clouds: true, snow: false, wind: true)
-a5 = Article.create!(user_id: user.id, article_type: "top", title: "top3", formality: 5, rain: true, clouds: true, snow: false, wind: true)
+#TOPS
+t1 = Article.create!(user_id: user.id, article_type: "top", title: "top1", formality: 1, rain: true, clouds: true, snow: false, wind: true)
+t2 = Article.create!(user_id: user.id, article_type: "top", title: "top2", formality: 2, rain: false, clouds: true, snow: false, wind: false)
+t3 = Article.create!(user_id: user.id, article_type: "top", title: "top3", formality: 3, rain: false, clouds: true, snow: false, wind: true)
+t4 = Article.create!(user_id: user.id, article_type: "top", title: "top4", formality: 4, rain: false, clouds: true, snow: false, wind: false)
+t5 = Article.create!(user_id: user.id, article_type: "top", title: "top5", formality: 5, rain: false, clouds: true, snow: false, wind: true)
+t6 = Article.create!(user_id: user.id, article_type: "top", title: "top6", formality: 7, rain: true, clouds: true, snow: false, wind: true)
+t7 = Article.create!(user_id: user.id, article_type: "top", title: "top7", formality: 9, rain: true, clouds: true, snow: false, wind: true)
+t8 = Article.create!(user_id: user.id, article_type: "top", title: "top8", formality: 10, rain: true, clouds: true, snow: false, wind: true)
+#BOTTOMS
+b1 = Article.create!(user_id: user.id, article_type: "bottom", title: "bottom1", formality: 1, rain: false, clouds: true, snow: false, wind: false)
+b2 = Article.create!(user_id: user.id, article_type: "bottom", title: "bottom2", formality: 2, rain: false, clouds: true, snow: false, wind: false)
+b3 = Article.create!(user_id: user.id, article_type: "bottom", title: "bottom3", formality: 3, rain: false, clouds: true, snow: false, wind: false)
+b4 = Article.create!(user_id: user.id, article_type: "bottom", title: "bottom4", formality: 4, rain: false, clouds: true, snow: false, wind: false)
+b5 = Article.create!(user_id: user.id, article_type: "bottom", title: "bottom5", formality: 8, rain: false, clouds: true, snow: false, wind: false)
+b6 = Article.create!(user_id: user.id, article_type: "bottom", title: "bottom6", formality: 10, rain: false, clouds: true, snow: false, wind: false)
+#OUTERWEAR
+o1 = Article.create!(user_id: user.id, article_type: "outerwear", title: "outerwear1", formality: 3, rain: false, clouds: true, snow: false, wind: true)
+o2 = Article.create!(user_id: user.id, article_type: "outerwear", title: "outerwear2", formality: 4, rain: false, clouds: true, snow: false, wind: false)
+o3 = Article.create!(user_id: user.id, article_type: "outerwear", title: "outerwear3", formality: 5, rain: true, clouds: true, snow: true, wind: true)
+o4 = Article.create!(user_id: user.id, article_type: "outerwear", title: "outerwear4", formality: 8, rain: true, clouds: true, snow: false, wind: true)
+#DRESSES
+d1 = Article.create!(user_id: user.id, article_type: "dress", title: "dress1", formality: 2, rain: false, clouds: true, snow: false, wind: false)
+d2 = Article.create!(user_id: user.id, article_type: "dress", title: "dress2", formality: 5, rain: false, clouds: true, snow: false, wind: false)
+d3 = Article.create!(user_id: user.id, article_type: "dress", title: "dress3", formality: 9, rain: false, clouds: true, snow: false, wind: false)
+#SHOES
+s1 = Article.create!(user_id: user.id, article_type: "shoes", title: "shoes1", formality: 1, rain: false, clouds: true, snow: false, wind: false)
+s2 = Article.create!(user_id: user.id, article_type: "shoes", title: "shoes2", formality: 5, rain: true, clouds: true, snow: false, wind: true)
+s3 = Article.create!(user_id: user.id, article_type: "shoes", title: "shoes3", formality: 10, rain: false, clouds: true, snow: false, wind: true)
 
-outfit = Outfit.create!(user_id: user.id, rain: true, clouds: true, snow: false, wind: false, formality: 4)
-Composition.create!(outfit_id: outfit.id, article_id: a1.id)
-Composition.create!(outfit_id: outfit.id, article_id: a2.id)
-Composition.create!(outfit_id: outfit.id, article_id: a3.id)
+
+#TOPS/BOTTOMS/OUTERWEAR/SHOES
+outfit1 = Outfit.create!(user_id: user.id, rain: true, clouds: true, snow: false, wind: false, formality: 1)
+Composition.create!(outfit_id: outfit1.id, article_id: t1.id)
+Composition.create!(outfit_id: outfit1.id, article_id: b1.id)
+Composition.create!(outfit_id: outfit1.id, article_id: o1.id)
+Composition.create!(outfit_id: outfit1.id, article_id: s1.id)
+
+outfit2 = Outfit.create!(user_id: user.id, rain: true, clouds: true, snow: false, wind: false, formality: 2)
+Composition.create!(outfit_id: outfit2.id, article_id: t2.id)
+Composition.create!(outfit_id: outfit2.id, article_id: b1.id)
+Composition.create!(outfit_id: outfit2.id, article_id: o1.id)
+Composition.create!(outfit_id: outfit2.id, article_id: s1.id)
+
+outfit3 = Outfit.create!(user_id: user.id, rain: true, clouds: true, snow: false, wind: false, formality: 3)
+Composition.create!(outfit_id: outfit3.id, article_id: t3.id)
+Composition.create!(outfit_id: outfit3.id, article_id: b2.id)
+Composition.create!(outfit_id: outfit3.id, article_id: o2.id)
+Composition.create!(outfit_id: outfit3.id, article_id: s1.id)
+
+outfit4 = Outfit.create!(user_id: user.id, rain: true, clouds: true, snow: false, wind: false, formality: 4)
+Composition.create!(outfit_id: outfit4.id, article_id: t4.id)
+Composition.create!(outfit_id: outfit4.id, article_id: b3.id)
+Composition.create!(outfit_id: outfit4.id, article_id: o2.id)
+Composition.create!(outfit_id: outfit4.id, article_id: s2.id)
+
+outfit5 = Outfit.create!(user_id: user.id, rain: true, clouds: true, snow: false, wind: false, formality: 6)
+Composition.create!(outfit_id: outfit5.id, article_id: t5.id)
+Composition.create!(outfit_id: outfit5.id, article_id: b4.id)
+Composition.create!(outfit_id: outfit5.id, article_id: o3.id)
+Composition.create!(outfit_id: outfit5.id, article_id: s2.id)
+
+outfit6 = Outfit.create!(user_id: user.id, rain: true, clouds: true, snow: false, wind: false, formality: 7)
+Composition.create!(outfit_id: outfit6.id, article_id: t6.id)
+Composition.create!(outfit_id: outfit6.id, article_id: b5.id)
+Composition.create!(outfit_id: outfit6.id, article_id: o3.id)
+Composition.create!(outfit_id: outfit6.id, article_id: s2.id)
+
+outfit7 = Outfit.create!(user_id: user.id, rain: true, clouds: true, snow: false, wind: false, formality: 9)
+Composition.create!(outfit_id: outfit7.id, article_id: t7.id)
+Composition.create!(outfit_id: outfit7.id, article_id: b5.id)
+Composition.create!(outfit_id: outfit7.id, article_id: o4.id)
+Composition.create!(outfit_id: outfit7.id, article_id: s2.id)
+
+outfit8 = Outfit.create!(user_id: user.id, rain: true, clouds: true, snow: false, wind: false, formality: 10)
+Composition.create!(outfit_id: outfit8.id, article_id: t8.id)
+Composition.create!(outfit_id: outfit8.id, article_id: b6.id)
+Composition.create!(outfit_id: outfit8.id, article_id: o4.id)
+Composition.create!(outfit_id: outfit8.id, article_id: s3.id)
+
+#DRESSES/SHOES
+outfit9 = Outfit.create!(user_id: user.id, rain: false, clouds: false, snow: false, wind: false, formality: 2)
+Composition.create!(outfit_id: outfit9.id, article_id: d1.id)
+Composition.create!(outfit_id: outfit9.id, article_id: s1.id)
+
+outfit10 = Outfit.create!(user_id: user.id, rain: false, clouds: true, snow: false, wind: false, formality: 5)
+Composition.create!(outfit_id: outfit10.id, article_id: d2.id)
+Composition.create!(outfit_id: outfit10.id, article_id: s2.id)
+
+outfit11 = Outfit.create!(user_id: user.id, rain: true, clouds: true, snow: false, wind: true, formality: 9)
+Composition.create!(outfit_id: outfit11.id, article_id: d3.id)
+Composition.create!(outfit_id: outfit11.id, article_id: s3.id)
