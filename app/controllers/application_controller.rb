7b7eb60f9d9fcd3e6908ec6753bce04e300bd469
Meredith Heller <meredith.a.heller@gmail.com>
2016-08-22 13:50:49 -0700

    sign in page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
 
   def current_user
     if session[:session_token]
-      @current_user = User.find_by(session: session[:session_token])
+      @current_user = User.find_by(session_token: session[:session_token])
     else
       nil
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: "static_pages#root"
   resources :users, only: [:new, :create]
-  resources :session, only: [:new, :create, :destroy]
+  resource :session, only: [:new, :create, :destroy]
 
   namespace :api, defaults: {format: :json} do
     resources :users, only: [:create, :update]
