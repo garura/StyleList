86870953e6303ef45515490a22b8efb102dec7a7
Meredith Heller <meredith.a.heller@gmail.com>
2016-08-22 11:49:58 -0700

    session controller and routes

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    @user = User.find_by_credentials(
+      params[:user][:email],
+      params[:user][:password]
+    )
+    if @user
+      log_in!(@user)
+      redirect_to root_url
+    else
+      flash.now[:errors] = ["Invalid username or password"]
+      render :new
+    end
+  end
+
+  def destroy
+    log_out!
+    render json: {}
+  end
+end
