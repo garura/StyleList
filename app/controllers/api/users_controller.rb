ae12414d6c91ae8a0ae910a5b2990ceb17e38617
Chris Jordan <chrisjordan.usa@gmail.com>
2016-08-22 13:44:55 -0700

    user controller stuff

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,4 +1,5 @@
 class Api::UsersController < ApplicationController
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -8,7 +9,6 @@ class Api::UsersController < ApplicationController
       render json: @user.errors.full_messages, status: 422
   end
 
-  # check consistency with server errors response
   def update
     @user = current_user
     if @user.update!(user_params)
