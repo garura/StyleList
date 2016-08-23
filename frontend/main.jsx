1f04a8b2a79b4e26943e79853c11a8a050259b71
Chris Jordan <chrisjordan.usa@gmail.com>
2016-08-23 02:06:47 -0700

    content id check

--- frontend/main.jsx
+++ frontend/main.jsx
@@ -4,7 +4,9 @@ import { render } from 'react-dom'
 import App from './containers/App'
 
 document.addEventListener("DOMContentLoaded", () => {
-  render(
-    <App/>, document.getElementById('content')
-    )
+  if (document.getElementById('content')) {
+    render(
+      <App/>, document.getElementById('content')
+      )
+  }
 });
